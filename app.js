39e0cad12a132cfcc5358b0ec83eab72fef767f0
Omar Curiel <omarcuriel@Omars-MacBook-Pro.local>
2018-05-17 15:19:12 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var app = express()
+
+app.set('view engine','ejs')
+
+app.get('/',function(req,res){
+  res.render('home')
+})
+
+app.get('/friends', function(req,res){
+  var friends = ['bob','taco','bell'];
+  res.render('friends', {friends: friends})
+})
+
+
+app.listen(3000, function(){
+  console.log('Server is running')
+})
